04350279209fadf090de2121daad808fcf137b58
alexw <alexwendland@me.com>
2022-10-16 19:25:28 -0500

    initial commit

---
+++ client/main.py
@@ -0,0 +1,57 @@
+from flask import Flask, abort, request, Response
+import asyncio
+import os
+import websockets
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+load_dotenv()
+WS_HOST = os.getenv('WS_HOST', 'localhost')
+WS_PORT = os.getenv('WS_PORT', 8765)
+if ':' in WS_HOST:
+    uri = f'ws://[{WS_HOST}]:{WS_PORT}/'
+else:
+    uri = f'ws://{WS_HOST}:{WS_PORT}/'
+
+
+with app.app_context():
+    # exit if there is no server to connect to
+    async def healthcheck():
+        async with websockets.connect(uri=uri) as conn:
+            await conn.ping()
+            
+    asyncio.run(healthcheck())
+    
+    
+    @app.route('/devices/<path:path>', methods=['GET'])
+    async def devices_list_all(path):
+        async with websockets.connect(uri=uri) as conn:
+            try:
+                await conn.send(request.path)
+                res = await conn.recv()
+
+                return Response(res, status=200, mimetype='application/json')
+                    
+            except websockets.ConnectionClosed as e:
+                print(e)
+            finally:
+                await conn.close()
+
+
+    @app.route('/power/<path:path>', methods=['GET'])
+    async def power_toggle_all(path):
+        async with websockets.connect(uri=uri) as conn:
+            try:
+                await conn.send(request.path)
+                res = await conn.recv()
+
+                return Response(res, status=200, mimetype='application/json')
+                    
+            except websockets.ConnectionClosed as e:
+                print(e)
+            finally:
+                await conn.close()
+
+
+if __name__ == '__main__':
+    app.run(port=8080)
---
+++ server/main.py
@@ -0,0 +1,118 @@
+import asyncio
+import socket
+import subprocess
+import re
+import os
+import kasa 
+import websockets
+import json
+import io
+import pprint
+import dotenv
+
+dotenv.load_dotenv()
+WS_HOST = os.getenv('WS_HOST')
+WS_PORT = os.getenv('WS_PORT')
+
+# identify eligible smart devices by the manufacturer prefix
+TPLink_MACaddr_re = r'54:af:97:a0:\w{2}:\w{2}'
+
+
+async def serve():
+    async with websockets.serve(handler, WS_HOST, WS_PORT):
+        await asyncio.Future()
+
+
+async def handler(webscocket):
+    async for message in webscocket:
+        print(message)
+        if message[0] != '/': continue
+
+        (noun,verb,target,*args) = message.lstrip('/').split('/')
+
+        if noun.lower() == 'power':
+            if verb.lower() == 'on': on = True
+            if verb.lower() == 'off': on = False
+            
+            if target.lower() == 'all':
+                res, err = await toggleAllDevices(on=on)
+                if err is None:
+                    await webscocket.send(res)
+                else:
+                    print(err)
+                    await webscocket.send(err.status)
+        
+        if noun.lower() == 'devices':
+            if verb.lower() == 'list':
+                if target.lower() == 'all' and args == []:
+                    res, err = await listAllDevices()
+                    if err is None:
+                        await webscocket.send(res)
+                    else:
+                        print(err)
+                        await webscocket.send(err.status)
+
+
+async def toggleAllDevices(on:bool) -> tuple[str,Exception]:
+    Devices = [ kasa.SmartPlug(i.get('ip_addr')) for i in get_TPLink_devices() ]
+    update_coros = [ i.update() for i in Devices ]
+    await asyncio.gather(*update_coros)
+
+    if on is True:
+        power_coros = [ i.turn_on() for i in Devices ]
+    else:
+        power_coros = [ i.turn_off() for i in Devices ]
+
+    await asyncio.gather(*power_coros)
+
+    devices_affected = [ {"alias": i._sys_info.get("alias"), "name": i._sys_info.get("dev_name"), "is_on": i.is_on} for i in Devices ]
+
+    return (
+        json.dumps(devices_affected), None
+        )
+
+
+async def listAllDevices() -> tuple[str,Exception]:
+    Devices = [ kasa.SmartPlug(i.get('ip_addr')) for i in get_TPLink_devices() ]
+    update_coros = [ i.update() for i in Devices ]
+    await asyncio.gather(*update_coros)
+
+    allDevices = [ {"alias": i._sys_info.get("alias"), "name": i._sys_info.get("dev_name"), "is_on": i.is_on} for i in Devices ]
+
+    return (
+        json.dumps(allDevices), None
+        )
+
+
+def get_TPLink_devices():
+    results = []
+    for i in subprocess.getoutput('ip neigh show').splitlines():
+        host = i.split(' ')
+        host.remove('')
+        
+        ip_returns = ['ip_addr', 'host_type', 'interface', 'ip_addr_type', 'mac_addr', 'status']
+
+        # validate ip_address
+        try:
+            socket.inet_aton(host[0])
+        except OSError:
+            continue
+
+        # validate MAC address
+        try:
+            if re.fullmatch(TPLink_MACaddr_re, host[4]):
+                results.append({
+                    'ip_addr': host[0],
+                    'mac_addr': host[4]
+                    })
+        
+        except IndexError:
+            continue
+        except Exception:
+            raise
+        
+    return results
+    
+
+if __name__ == '__main__':
+    asyncio.run(serve())
