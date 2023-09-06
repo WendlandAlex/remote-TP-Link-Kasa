6796b71ff997ed74b75cd75038b7230567dea76f
alexw <alexwendland@me.com>
2023-09-06 07:59:58 -0500

    delete flask frontend

--- client/client/services/mfa.py
+++
@@ -1,7 +0,0 @@
-def validate_totp(totp_object, mfa_code) -> bool:
-    print(
-        totp_object.now(), mfa_code, totp_object.verify(int(mfa_code))
-    )
-    return totp_object.verify(
-        int(mfa_code)
-        )
